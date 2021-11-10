0962def9187303e428b41ec408ae7afd0f8e18a8
tobeabetterstudent <1907675978@qq.com>
2021-11-10 15:36:56 +0800

    first example of gin

--- Gin/example/example.go
+++ Gin/example/example.go
@@ -1,6 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"fmt"
+)
+
+
+// 直接使用net/http包来处理http请求
+func demoByNet() {
+	// 注册一个路由('/') 及这个路由的handler到DefaultServeMux中
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello World"))
+    })
+	// ln, err := net.Listen("tcp", addr)做了初试化了socket, bind, listen的操作.
+	// rw, e := l.Accept()进行accept, 等待客户端进行连接
+	// go c.serve(ctx) 启动新的goroutine来处理本次请求. 同时主goroutine继续等待客户端连接, 进行高并发操作
+	// h, _ := mux.Handler(r) 获取注册的路由, 然后拿到这个路由的handler, 然后将处理结果返回给客户端
+    if err := http.ListenAndServe(":8000", nil); err != nil {
+        fmt.Println("start http server fail:", err)
+    }
+}
 
 
 func main() {
